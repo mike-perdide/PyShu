bfbaed1314ef11ba7eb17008a8e8857b0ef52365
Julien Miotte <mike.perdide@gmail.com>
2012-01-24 15:53:53 +0100

    Moving all the methods to the pyshu lib.

--- proto2.py
+++ proto2.py
@@ -1,117 +1,13 @@
 import pickle
-from pyshu import snd_record, process_fft, graph_side_to_side, graph_one, FFT_STEP, RATE, play_array
+from pyshu.sound import snd_record, play_array
+from pyshu.calculus import process_fft
+from pyshu.graphs import graph_side_to_side, graph_one
+from pyshu.defaults import FFT_STEP, RATE
+from pyshu.utils import detect_word_edges, find_most, center_and_expand, record_or_load
 from pylab import *
 from sklearn.cluster import KMeans
 from sklearn.svm import SVC
 from IPython import embed
-
-
-def fit_model(fft_data):
-    model = KMeans(k=20)
-    model.fit(fft_data)
-    return model
-
-def record_or_load(filename, message, length=5):
-    try:
-        with open(filename) as handle:
-            recording = np.load(handle)
-    except:
-        raw_input(message + " Hit enter when ready. [Go!]")
-        recording = snd_record(record_seconds=length)
-
-        choice = raw_input("Display recording ? [Yn]")
-        if not choice or choice[0] != 'n':
-            plt.plot(recording)
-            plt.show()
-
-        choice = raw_input("Save recording ? [Yn]")
-        if not choice or choice[0] != 'n':
-            with open(filename, "w") as handle:
-                np.save(handle, recording)
-
-    return recording
-
-def replace(_array, _replace_array):
-    new_array = np.copy(_array)
-    for key in _replace_array:
-        new_array[_array==key] = 0
-
-    return new_array
-
-def all_recordings():
-    ambiant_noises = record_or_load("five_sec_ambiant.wav",
-                                    "Recording 5 seconds of ambiant noises")
-
-    ten_sec_r2 = record_or_load("ten_sec_r2.wav",
-                                "Recording 10 seconds of you saying 'R2'"
-                                "(6-10 times).", 10)
-
-    ten_sec_open = record_or_load("ten_sec_open.wav",
-                                  "Recording 10 seconds of you saying 'open'"
-                                  "(6-10 times).", 10)
-
-    return ambiant_noises, ten_sec_r2, ten_sec_open
-
-def all_fft(records):
-    ambiant_noises, ten_sec_r2, ten_sec_open = records
-
-    fft_ambiant = process_fft(ambiant_noises)
-    fft_r2 = process_fft(ten_sec_r2)
-    fft_open = process_fft(ten_sec_open)
-
-    return fft_ambiant, fft_r2, fft_open
-
-def center_and_expand(_array, new_length):
-    new_array = zeros(new_length)
-    for i in xrange(len(_array)):
-        new_array[int(double(i)/len(_array)*new_length)] = _array[i]
-
-    return new_array
-
-SIGNIFIANT_LEAP = int(0.25 * RATE/FFT_STEP)
-
-def detect_word_edges(ambiant_groups, fft_data, offset=0):
-    # Other way to detect edges : we should try unsupervised learning with 2
-    # groups.
-    edges = []
-    start = 0
-#    print ambiant_groups
-
-    len_data = len(fft_data)
-
-    logfile = open("logfile", "w")
-    for i in xrange(len_data - SIGNIFIANT_LEAP):
-        group_count = bincount(fft_data[i:i+SIGNIFIANT_LEAP])
-        ambiant_count = 0
-        for group in ambiant_groups:
-            if group < len(group_count):
-                ambiant_count += group_count[group]
-
-
-        is_ambiant = ambiant_count > .6 * SIGNIFIANT_LEAP
-
-        logfile.write("[%d:%d], %s, count: %d\n" % (
-            (offset + i) * FFT_STEP,
-            (offset + i + SIGNIFIANT_LEAP) * FFT_STEP,
-            repr(group_count),
-            ambiant_count)
-        )
-
-        if not (start or is_ambiant):
-            # This isn't ambiant noise
-            # We haven't recorded the start of the word yet
-            start = i + offset
-        elif start and is_ambiant:
-            edges.append((start, i + offset))
-            start = 0
-
-    logfile.close()
-
-    return edges
-
-def find_most(data):
-    count = bincount(data)
-    return find(count > (.1 * sum(count)))
 
 
 if __name__ == "__main__":
