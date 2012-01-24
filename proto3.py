3f63fe44cf9986a1ab09f8af0d27d3ef6151ae2c
Julien Miotte <mike.perdide@gmail.com>
2012-01-24 15:55:11 +0100

    Starting to work on the new prototype (mk 3)

---
+++ proto3.py
@@ -0,0 +1,11 @@
+
+
+
+if __name__ == "__main__":
+    # Record sound
+    # Filter using human speech known frequencies
+    # Filter-out the hight frequencies
+    # Detect the word edges
+    # On each word, do a fft decomposition
+    # Use unsupervised model to group fft decompositions
+    # Use supervised model to learn each sequence of fft decompositions groups
